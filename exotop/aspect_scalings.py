7060835ed4746335e3e8164d27cac097ba67ef90
clairesworld <claire.guimond@gmail.com>
2020-11-13 17:38:41 +0000

    same

--- exotop/aspect_scalings.py
+++ exotop/aspect_scalings.py
@@ -150,7 +150,7 @@ def process_at_solutions(case, postprocess_functions, dat=None, t1=0, data_path=
             print('ts', ts)
             for fn in postprocess_functions:
                 new_params_dict = fn(case, n=n, ts=ts, dat=dat, **kwargs)
-                print('new params dict', new_params_dict)
+                print('new params dict', new_params_dict.keys(), 'len', len(new_params_dict))
                 new_params = pd.DataFrame(new_params_dict, index=[ts])
                 df_to_extend = pd.concat([df_to_extend, new_params])
                 print('    Calculated', fn, 'for solution', n, '/', int(n_quasi[-1]))
