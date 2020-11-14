acf8bd7d2c4933bd2e559479db741949b3cdfd9c
clairesworld <claire.guimond@gmail.com>
2020-11-14 11:46:51 +0000

    same

--- exotop/setup_postprocessing.py
+++ exotop/setup_postprocessing.py
@@ -2,9 +2,6 @@ import numpy as np
 
 Ra_ls = ['1e6', '3e6', '1e7', '3e7', '1e8', '3e8']  # 3e5 no convection
 eta_ls = ['1e5', '1e6', '1e7', '1e8']
-end = np.empty_like(t1, dtype=object)
-end[:] = "-wide"
-end[2:, 5] = "-wide-ascii"  # which runs use ascii initialisation
 
 # Ra from 1e6 to 3e8
 t1 = np.array([[0.5, 0.3, 0.25, 0.7, 0.2, 0.055],  # eta 1e5
@@ -18,6 +15,10 @@ regime_grid = np.array([['steady', 'steady' , 'steady', 'chaotic', 'sluggish', '
                ['no convection', 'steady', 'steady', 'transitional', 'chaotic', 'not converged'], # eta 1e7
                ['no convection', 'no convection', 'steady', 'transitional', 'not converged', 'not converged']])  # eta 1e8
 
+# case names
+end = np.empty_like(t1, dtype=object)
+end[:] = "-wide"
+end[2:, 5] = "-wide-ascii"  # which runs use ascii initialisation
 
 data_path_bullard = '/raid1/cmg76/aspect/model-output/'
 fig_path_bullard = '/raid1/cmg76/aspect/figs/'
