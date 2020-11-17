7a5b6ee2e6e23b2205649a20d01ced269db3335b
clairesworld <claire.guimond@gmail.com>
2020-11-17 12:16:11 +0000

    same

--- exotop/aspect_scalings.py
+++ exotop/aspect_scalings.py
@@ -113,7 +113,7 @@ def pickle_remove_duplicate(case, suffix, which='sol', fend='.pkl', data_path=da
     if os.path.exists(case_path + 'pickle/' + fname):
         df = pkl.load(open(case_path + 'pickle/' + fname, 'rb'))
         print('\n\ndf', df)
-        series = df.loc[which]
+        series = df[which]
         unique = series.duplicated()  # boolean array of duplicates
         df_new = df[unique]
         print('\n\ndf_new', df_new)
