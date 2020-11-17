5cb0a2319af23670939974c49db9f84f06cc314b
clairesworld <claire.guimond@gmail.com>
2020-11-17 17:23:16 +0000

    same

--- exotop/summary_plots.py
+++ exotop/summary_plots.py
@@ -48,6 +48,6 @@ from setup_postprocessing import Ra_ls, eta_ls, t1, end, data_path, fig_path, c_
 
 case = 'Ra3e8-eta1e5-wide'
 sc.print_solution_data(case, suffix='_T', keys=['sol', 'time', 'delta_rh'], data_path=data_path)
-sc.print_solution_data(case, suffix='_h_all', keys=['sol', 'time', 'delta_rh'], data_path=data_path)
+sc.print_solution_data(case, suffix='_h_all', keys=['time', 'h_rms'], data_path=data_path)
 
 print('Summary plots complete')
