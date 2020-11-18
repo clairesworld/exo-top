1dc6530207645d137ba439ab66bb92db9f25c3cd
clairesworld <claire.guimond@gmail.com>
2020-11-18 16:33:24 +0000

    same

--- exotop/summary_plots.py
+++ exotop/summary_plots.py
@@ -12,17 +12,17 @@ load = 'auto'
 
 ## plot summaries across delta eta
 
-i_plot = list(range(len(eta_ls)))
-for ii, eta in enumerate(eta_ls):  # across eta_ls
-    if ii in i_plot:
-        cases_ii = ['Ra' + Ra + '-eta' + eta + e for Ra, e in zip(Ra_ls, end[ii])]
-        labels_ii = ['Ra=' + Ra for Ra in Ra_ls]
-        sc.subplots_cases(
-            cases_ii, labels=labels_ii, t1=t1[ii], save=True, load=load,
-            fname='all-eta' + eta, suptitle='$\Delta \eta$ = ' + eta,
-            includepdf=True, includeTz=True, show_sols=True,  # set False for faster summary with stats only
-            includegraphic=True, data_path=data_path, fig_path=fig_path, fig_fmt=fig_fmt, regime_grid=regime_grid[ii],
-        )
+# i_plot = list(range(len(eta_ls)))
+# for ii, eta in enumerate(eta_ls):  # across eta_ls
+#     if ii in i_plot:
+#         cases_ii = ['Ra' + Ra + '-eta' + eta + e for Ra, e in zip(Ra_ls, end[ii])]
+#         labels_ii = ['Ra=' + Ra for Ra in Ra_ls]
+#         sc.subplots_cases(
+#             cases_ii, labels=labels_ii, t1=t1[ii], save=True, load=load,
+#             fname='all-eta' + eta, suptitle='$\Delta \eta$ = ' + eta,
+#             includepdf=True, includeTz=True, show_sols=True,  # set False for faster summary with stats only
+#             includegraphic=True, data_path=data_path, fig_path=fig_path, fig_fmt=fig_fmt, regime_grid=regime_grid[ii],
+#         )
 
 # compare 64 and 129 resolution for Ra=3e7
 # fig, ax = sc.case_subplots(
@@ -49,7 +49,7 @@ for ii, eta in enumerate(eta_ls):  # across eta_ls
 
 ## look at individual case data
 #
-# case = 'Ra1e7-eta1e5-wide'
-# sc.print_solution_data(case, suffix='_h_all', keys=None, data_path=data_path)
+case = 'Ra3e8-eta1e5-wide'
+sc.print_solution_data(case, suffix='_sol', keys=None, data_path=data_path)
 
 print('Summary plots complete')
