0ffa331619414cdf8f67398884d9f29798c29a98
clairesworld <claire.guimond@gmail.com>
2020-11-20 15:17:21 +0000

    same

--- exotop/aspect_scalings.py
+++ exotop/aspect_scalings.py
@@ -1489,7 +1489,6 @@ def lid_mobility_at_sol(case=None, dat=None, n=None, data_path=data_path_bullard
     df_T = pickleio(case, '_T', [T_parameters_at_sol], dat_new=dat, at_sol=True,
                     data_path=data_path, **kwargs)
     df_sol = df_T.set_index('sol')
-    print('df', df_sol, 'loc[n, delta_0]', df_sol.loc[n, 'delta_0'])
     S = dat.surface_mobility(n=n, delta_0=df_sol.loc[n, 'delta_0'], delta_rh=df_sol.loc[n, 'delta_rh'],
                              delta_l=df_sol.loc[n, 'delta_L'])
     return S
