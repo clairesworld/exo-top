b9d096af2124d5cd2b890d678758efcfade5bf07
clairesworld <claire.guimond@gmail.com>
2020-12-01 15:01:33 +0000

    k

--- exotop/scaling_plots.py
+++ exotop/scaling_plots.py
@@ -12,7 +12,7 @@ load = load_grid
 
 sc.subplots_topo_regimes(Ra_ls, eta_ls, regime_grid, regime_names, c_regimes=c_regimes, save=True, t1=t1_grid, load=load,
                          fig_path=fig_path, fname='h_Ra_all', fig_fmt=fig_fmt, end=end_grid, labelsize=14, xlabel='Ra',
-                         ylabel='dynamic topography (km)', xlabelpad=8, ylabelpad=10, fit=True,
+                         ylabel='dynamic topography (km)', xlabelpad=8, ylabelpad=12, fit=True,
                          xlim=(0.6e6, 4e8), ylim=(1, 12), logx=True, logy=True, hscale=2e-5 * 2700 * 2890,
                          regimes_title='Time-dependence', data_path=data_path)
 
@@ -22,7 +22,7 @@ sc.subplots_topo_regimes(Ra_ls, eta_ls, regime_grid, regime_names, c_regimes=c_r
                          T_components=True,
                          load=load, fig_path=fig_path, fname='h_T_all', fig_fmt=fig_fmt, end=end_grid,
                          labelsize=14, xlabel=r'$\alpha \delta_{rh} \Delta T_{rh}$', ylabel='dynamic topography',
-                         xlabelpad=8, ylabelpad=14, fit=True, alpha_m=alpha_m, showallscatter=True,
+                         xlabelpad=8, ylabelpad=10, fit=True, alpha_m=alpha_m, showallscatter=True,
                          xlim=(1e-8, 1e-6), ylim=(7e-3, 7.5e-2), logx=True, logy=True,
                          regimes_title='Time-dependence', data_path=data_path)
 
