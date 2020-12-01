1b9fbf324bd7e9cea63a4b10f2dd1f66847eb4db
clairesworld <claire.guimond@gmail.com>
2020-12-01 20:58:01 +0000

    k

--- exotop/scaling_plots.py
+++ exotop/scaling_plots.py
@@ -12,7 +12,7 @@ load = True # load_grid
 
 sc.subplots_topo_regimes(Ra_ls, eta_ls, regime_grid_td, regime_names_td, c_regimes=c_regimes_td, save=True, t1=t1_grid, load=load,
                          fig_path=fig_path, fname='h_Ra_all', fig_fmt=fig_fmt, end=end_grid, labelsize=14, xlabel='Ra',
-                         ylabel='dynamic topography (km)', xlabelpad=8, ylabelpad=-2, fit=True, showallscatter=True,
+                         ylabel='dynamic topography (km)', xlabelpad=8, ylabelpad=-2, fit=True, showallscatter=False,
                          xlim=(0.6e6, 5e8), ylim=(1, 12), logx=True, logy=True, hscale=2e-5 * 2700 * 2890,
                          regimes_title='Time-dependence', leftleg_bbox=(-0.01, 0.95), data_path=data_path)
 load = True
@@ -32,7 +32,7 @@ sc.subplots_topo_regimes(Ra_ls, eta_ls, regime_grid_td, regime_names_td, c_regim
 sc.subplots_Ra_scaling(Ra_ls, eta_ls, t1=t1_grid, end=end_grid, keys=['Nu', 'delta_0', 'delta_rh', 'T_i'], data_path=data_path,
                        fig_path=fig_path, load=load, save=True, fname='delta-Nu-Ti', xlim=(1e5, 5e8),
                        ylim=[(None),(None),(None),(0.8,1)], labelsize=14,
-                       ylabels=['Nu', r'$\delta_0$', r'$\delta_rh$', r'$T_i$'], psuffixes=['_T', '_Nu'],
+                       ylabels=['Nu', r'$\delta_0$', r'$\delta_{rh}$', r'$T_i$'], psuffixes=['_T', '_Nu'],
                        postprocess_functions=[sc.T_parameters_at_sol, sc.Nu_at_ts], Ra_i=True,
                        compare_label='Moresi & Solomatov 1995', compare_pub=sc.moresi95,
                        fig_fmt=fig_fmt, cmap='winter', fit=True)
