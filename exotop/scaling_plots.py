991d0783c1eb8e6569ae54f4de05fbebef365634
clairesworld <claire.guimond@gmail.com>
2020-12-05 19:50:19 +0000

    k

--- exotop/scaling_plots.py
+++ exotop/scaling_plots.py
@@ -32,12 +32,13 @@ load = True
 #
 sc.subplots_topo_regimes(Ra_ls, eta_ls, regime_grid_td, regime_names_td, c_regimes=c_regimes_td, save=True, t1=t1_grid,
                          load=load, show_isoviscous=True, averagefirst=True, alpha_m=alpha_m,
-                         fig_path=fig_path, fname='h_Ra_all', fig_fmt=fig_fmt, end=end_grid, labelsize=14, xlabel='Ra',
+                         fig_path=fig_path, fname='h_Ra_all', fig_fmt=fig_fmt, end=end_grid, labelsize=14,
+                         xlabel='Ra_i', Ra_i=True,
                          ylabel='dynamic topography $h^\prime$', y2label='dynamic topography $h$ (km)',
                          xlabelpad=8, ylabelpad=5, fit=True, showallscatter=False,
                          xlim=(0.6e6, 5e8), #ylim=(1, 12),
                          logx=True, logy=True,
-                         p_dimensionals=p_Earth, Ra_i=True,
+                         p_dimensionals=p_Earth,
                          regimes_title='Time-dependence', leftleg_bbox=(-0.01, 0.95), data_path=data_path)
 
 
