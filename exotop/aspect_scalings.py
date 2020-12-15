aa295a9bebe622747daa83ef96ca4b9ffc313672
clairesworld <claire.guimond@gmail.com>
2020-12-15 14:53:06 +0000

    k

--- exotop/aspect_scalings.py
+++ exotop/aspect_scalings.py
@@ -2024,7 +2024,7 @@ def plot_heuristic_scalings(Ra_ls, eta_ls, regime_grid=None, t1=None, load=None,
 
     ax.set_xscale('log')
     ax.set_yscale('log')
-    ax.axis('equal', 'box')
+    ax.set_aspect('equal', 'box')
     if ylim is not None:
         ax.set_ylim(ylim)
         ax.set_xlim(ylim)
