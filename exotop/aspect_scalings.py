f22b36acdbf77c48602b2d40f68397dfc06f1382
clairesworld <claire.guimond@gmail.com>
2020-12-15 15:41:32 +0000

    k

--- exotop/aspect_scalings.py
+++ exotop/aspect_scalings.py
@@ -2050,12 +2050,12 @@ def plot_heuristic_scalings(Ra_ls, eta_ls, regime_grid=None, t1=None, load=None,
 
     fig, ax = plot_error_contours(fig, ax)
 
-    if not (not cbar):
-        cbar = colourbar(scat, label=clabel, ticklabels=cticklabels, labelsize=labelsize)
-        # if cbar == 'regime':
-        #     nlabels = len(cticklabels)
-        #     tick_locs = (np.arange(vmin, vmax + 1) + 0.5) * (nlabels - 1) / nlabels
-        #     cbar.set_ticks(tick_locs)
+    # if not (not cbar):
+    #     cbar = colourbar(scat, label=clabel, ticklabels=cticklabels, labelsize=labelsize)
+    #     if cbar == 'regime':
+    #         nlabels = len(cticklabels)
+    #         tick_locs = (np.arange(vmin, vmax + 1) + 0.5) * (nlabels - 1) / nlabels
+    #         cbar.set_ticks(tick_locs)
 
     if save:
         plot_save(fig, fname, **kwargs)
