075f181f8c807dabe9a798976a583d005924189a
clairesworld <claire.guimond@gmail.com>
2021-01-04 14:15:42 +0000

    k

--- exotop/1D_topo_plots.py
+++ exotop/1D_topo_plots.py
@@ -2,7 +2,8 @@
 
 import sys
 sys.path.insert(0, '/home/cmg76/Works/exo-top/')
-from exotop.setup_postprocessing import Ra_ls, eta_ls, t1_grid, end_grid, data_path, fig_path, c_rms, c_peak, c_regimes_td, fig_fmt, \
+from exotop.setup_postprocessing import Ra_ls, eta_ls, t1_grid, end_grid, data_path, fig_path, c_rms, c_peak, \
+    c_regimes_td, fig_fmt, \
     regime_grid_td, regime_names_td, load_grid, alpha_m, p_Earth  # noqa: E402
 from exotop import aspect_scalings as sc  # noqa: E402
 from exotop.model_1D import plot_top as pltop  # noqa: E402
--- exotop/heuristic_scaling_plots.py
+++ exotop/heuristic_scaling_plots.py
@@ -2,9 +2,10 @@
 
 import sys
 sys.path.insert(0, '/home/cmg76/Works/exo-top/')
-from exotop import aspect_scalings as sc  # noqa: E402
-from exotop.setup_postprocessing import Ra_ls, eta_ls, t1_grid, end_grid, data_path, fig_path, c_rms, c_peak, c_regimes_td, fig_fmt, \
+from exotop.setup_postprocessing import Ra_ls, eta_ls, t1_grid, end_grid, data_path, fig_path, c_rms, c_peak, \
+    c_regimes_td, fig_fmt, \
     regime_grid_td, regime_names_td, load_grid, alpha_m, p_Earth  # noqa: E402
+from exotop import aspect_scalings as sc  # noqa: E402
 
 
 load = load_grid
