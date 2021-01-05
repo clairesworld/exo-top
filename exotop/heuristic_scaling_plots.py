50013b31d1bf149f4793ca06dcb6e18a79f465fa
clairesworld <claire.guimond@gmail.com>
2021-01-05 19:55:12 +0000

    k

--- exotop/heuristic_scaling_plots.py
+++ exotop/heuristic_scaling_plots.py
@@ -15,11 +15,11 @@ load = True #load_grid
 const, expon = sc.plot_heuristic_scalings(Ra_ls, eta_ls, regime_grid=regime_grid_td, t1=t1_grid, load=load, end=end_grid,
                            literature_file=None, legend=True, cbar='eta', alpha_m=alpha_m, intercept=True,
                             c='k', which_h='rms', data_path=data_path, include_regimes=['chaotic'],
-                            save=True, fname='model-data-chaotic', ylim=[6e-3, 4e-2])
+                            save=True, fname='model-data-chaotic', ylim=[4e-3, 4e-2])
 print('fit parameters:', const, expon)
 
 sc.plot_heuristic_scalings(Ra_ls, eta_ls, regime_grid=regime_grid_td, t1=t1_grid, load=load, end=end_grid,
                            literature_file=None, legend=True, cbar='regime', clist=c_regimes_td, intercept=True,
                            regime_names=regime_names_td, alpha_m=alpha_m,
                             c='k', which_h='rms', data_path=data_path, #include_regimes=['chaotic'],
-                            save=True, fname='model-data-regimes', ylim=[6e-3, 4e-2])
+                            save=True, fname='model-data-regimes', ylim=[2e-3, 4e-2])
