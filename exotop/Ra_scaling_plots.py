7c22428e9f42f35455208a9eefca875c74adcd03
clairesworld <claire.guimond@gmail.com>
2021-01-08 09:52:12 +0000

    k

--- exotop/Ra_scaling_plots.py
+++ exotop/Ra_scaling_plots.py
@@ -36,20 +36,21 @@ load = True # load_grid
 sc.subplots_Ra_scaling(Ra_ls[4:], eta_ls[1:], t1_grid=t1_grid[1:, 4:], end_grid=end_grid[1:, 4:],
                        keys=['delta_rh', 'dT_rh', 'Nu'], data_path=data_path, fig_path=fig_path,
                        load_grid=load,# load_grid[1:, 4:],
-                       Ra_i='eff', save=True, fname='delta_rh-Nu-chaotic-eff',
+                       Ra_i='eff', save=True, fname='Ra-scaling-chaotic-eff',
                        labelsize=14, ylabels=[r'$\delta_{rh}$', r'$\Delta T_{rh}$', 'Nu'], psuffixes=['_T', '_Nu'],
                        title='Chaotic time-dependence', postprocess_functions=[sc.T_parameters_at_sol, sc.Nu_at_ts],
                        xlim=(0.7e6, 4e7), ylim=[(None), (None), (None)],
-                       legloc=['lower left', 'upper left', 'upper left'], cmap='winter', compare_pub=sc.moresi95,
+                       legloc=['lower left', 'upper left', 'upper left'], cmap='winter',
+                       #compare_pub=sc.moresi95,
                        compare_label='', fig_fmt=fig_fmt, fit=True, postprocess_kwargs=postprocess_kwargs)
-
-sc.subplots_Ra_scaling(Ra_ls[:3], eta_ls, t1_grid=t1_grid[:, :3], end_grid=end_grid[:, :3], keys=['delta_rh', 'dT_rh'],
-                       data_path=data_path, fig_path=fig_path, load_grid=load, Ra_i='eff', save=True,
-                       fname='delta_rh-steady-eff', labelsize=14, ylabels=[r'$\delta_{rh}$', r'$\Delta T_{rh}$'],
-                       psuffixes=['_T'], title='Steady-state', postprocess_functions=[sc.T_parameters_at_sol],
-                       xlim=(0.3e5, 1e6), ylim=[(None), (None)], legloc=['lower left', 'upper left'], cmap='winter',
-                       compare_pub=sc.moresi95, compare_label='', fig_fmt=fig_fmt, fit=True,
-                       postprocess_kwargs=postprocess_kwargs)
+#
+# sc.subplots_Ra_scaling(Ra_ls[:3], eta_ls, t1_grid=t1_grid[:, :3], end_grid=end_grid[:, :3], keys=['delta_rh', 'dT_rh'],
+#                        data_path=data_path, fig_path=fig_path, load_grid=load, Ra_i='eff', save=True,
+#                        fname='delta_rh-steady-eff', labelsize=14, ylabels=[r'$\delta_{rh}$', r'$\Delta T_{rh}$'],
+#                        psuffixes=['_T'], title='Steady-state', postprocess_functions=[sc.T_parameters_at_sol],
+#                        xlim=(0.3e5, 1e6), ylim=[(None), (None)], legloc=['lower left', 'upper left'], cmap='winter',
+#                        compare_pub=sc.moresi95, compare_label='', fig_fmt=fig_fmt, fit=True,
+#                        postprocess_kwargs=postprocess_kwargs)
 
 
 
@@ -57,7 +58,7 @@ sc.subplots_Ra_scaling(Ra_ls[:3], eta_ls, t1_grid=t1_grid[:, :3], end_grid=end_g
 
 sc.subplots_Ra_scaling(Ra_ls[4:], eta_ls[1:], t1_grid=t1_grid[1:, 4:], end_grid=end_grid[1:, 4:],
                        keys=['delta_0', 'delta_rh', 'dT_rh', 'Nu'], data_path=data_path, fig_path=fig_path, load_grid=load,
-                       Ra_i=True, save=True, fname='delta_rh-Nu-chaotic', compare_label='Moresi & Solomatov 1995', labelsize=14,
+                       Ra_i=True, save=True, fname='Ra-scaling-chaotic', compare_label='Moresi & Solomatov 1995', labelsize=14,
                        ylabels=[r'$\delta_{0}$', r'$\delta_{rh}$', r'$\Delta T_{rh}$', 'Nu'], psuffixes=['_T', '_Nu'],
                        title='Chaotic time-dependence', postprocess_functions=[sc.T_parameters_at_sol, sc.Nu_at_ts],
                        xlim=(1e7, 2e8), #ylim=[(None), (None), (None)],
