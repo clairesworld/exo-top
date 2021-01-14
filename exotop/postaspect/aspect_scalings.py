59de10f8f13e0e9ed1b75338a15289cb17410e45
clairesworld <claire.guimond@gmail.com>
2021-01-14 13:39:04 +0000

    k

--- exotop/postaspect/aspect_scalings.py
+++ exotop/postaspect/aspect_scalings.py
@@ -2034,20 +2034,21 @@ def plot_fit_parameter_grid(Ra_ls, eta_ls,  data_path=data_path_bullard, fig_pat
     # normalize to axis limits
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
-    xlim2 = np.array([Ra.min(), Ra.max()]) - xlim[0] / (xlim[1] - xlim[0])
-    ylim2 = np.array([eta.min(), eta.max()]) - ylim[0] / (ylim[1] - ylim[0])
+    xlim2 = np.log10(np.array([Ra.min(), Ra.max()])) - xlim[0] / (xlim[1] - xlim[0])
+    ylim2 = np.log10(np.array([eta.min(), eta.max()])) - ylim[0] / (ylim[1] - ylim[0])
 
     X2, Y2 = np.meshgrid(np.logspace(np.log10(xlim2[0]), np.log10(xlim2[1])),
                          np.logspace(np.log10(ylim2[0]), np.log10(ylim2[1])))
-
-    print('xlim2', xlim2)
-    print('ylim2', ylim2)
 
     print('X min', X.min())
     print('Y min', Y.min())
 
     print('xlim for norm', xlim)
     print('ylim for norm', ylim)
+
+    print('xlim2', xlim2)
+    print('ylim2', ylim2)
+
 
     print('X2 min', X2.min())
     print('Y2 min', Y2.min())
