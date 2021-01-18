7379e4c6f3f24412c6118fb002a6d0447a35e314
clairesworld <claire.guimond@gmail.com>
2021-01-18 21:08:16 +0000

    k

--- exotop/postaspect/aspect_scalings.py
+++ exotop/postaspect/aspect_scalings.py
@@ -1155,9 +1155,9 @@ def plot_h_vs(Ra=None, eta=None, t1_grid=None, end_grid=None, load_grid='auto', 
                 # calculate Mahalanobis distance for chi square later
                 div = int(np.ceil(len(h_rms_all)/len(x_all)))
                 data = pd.DataFrame({'y': np.log10(h_rms_all[::div]), 'x': np.log10(x_all)})
-                d_m = mahalanobis(x=data, data=data, cov=None)
+                V = np.cov(np.array([np.log10(h_rms_all[::div]), np.log10(x_all)]).T)
+                d_m = mahalanobis(x=data, data=data, cov=V)
                 D_m2 = np.mean(d_m ** 2)
-                print('mean of D_m**2', D_m2)
                 D_m2_all.append(D_m2)
 
                 # append to working
