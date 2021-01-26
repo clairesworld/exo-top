d600bb96815436ff058a4f8159e6a0dab42836a3
clairesworld <claire.guimond@gmail.com>
2021-01-26 20:09:07 +0000

    k

--- exotop/slides_plots.py
+++ exotop/slides_plots.py
@@ -17,18 +17,18 @@ if regimes == 'all':
     include_regimes = ['steady', 'trans.', 'chaotic']
     ylim = [6e-3, 4e-2]
     xlim = [0.7e5, 3e7]
-    yticks = [6e-3, 1e-2, 2e-2]
-    xticks = [1e6, 1e7]
+    yticks = [6e-3, 1e-2, 4e-2]
+    xticks = [1e5, 1e6, 1e7]
     c_rms = ['xkcd:lime green', 'xkcd:lilac', 'xkcd:orange', 'xkcd:yellow']
 elif regimes == 'chaotic':
     include_regimes = ['steady', 'trans.', 'chaotic']
-    ylim = ylim_ch
-    xlim = xlim_ch
-    yticks = yticks_ch
-    xticks = xticks_ch
+    ylim = [6e-3, 2e-2]
+    xlim = [1e6, 3e7]
+    yticks = [6e-3, 1e-2, 4e-2]
+    xticks = [1e6, 1e7]
     c_rms = ['xkcd:lilac', 'xkcd:orange', 'xkcd:yellow']
 
-fig, ax, ax2 = sc.plot_h_vs(Ra=Ra_ls, eta=eta_ls, t1_grid=t1_grid, end_grid=end_grid, load_grid=True, data_path=data_path,
+fig, ax = sc.plot_h_vs(Ra=Ra_ls, eta=eta_ls, t1_grid=t1_grid, end_grid=end_grid, load_grid=True, data_path=data_path,
                  fig_path=fig_path, averagescheme='timefirst', which_x='Ra_i_eff',
                  sigma=1,
                  include_regimes=regimes, save=False, fname='h_Raieff_chaotic_timeavg', labelsize=axissize,
@@ -38,10 +38,9 @@ fig, ax, ax2 = sc.plot_h_vs(Ra=Ra_ls, eta=eta_ls, t1_grid=t1_grid, end_grid=end_
                  c_peak='k', c_rms=c_rms,
                        fit=True, logx=True, logy=True, hscale=1,
                  show_isoviscous=False, ylim=ylim, xlim=xlim, postprocess_kwargs=postprocess_kwargs,
-                 regime_grid=regime_grid_td, p_dimensionals=p_Earth)
+                 regime_grid=regime_grid_td, p_dimensionals=None)
 
 ax.tick_params(axis='x', labelsize=ticksize, pad=15)
-ax2.tick_params(axis='y', labelsize=ticksize, pad=15)
 ax.tick_params(axis='y', labelsize=ticksize, pad=15)
 if yticks is not None:
     ax.set_yticks(yticks)
@@ -62,5 +61,5 @@ handles.append(mlines.Line2D([], [], color=c_rms[3], marker='o', ls='--',
 
 ax.legend(handles=handles, frameon=False, fontsize=25, ncol=1, bbox_to_anchor=(1.05, 1), loc='upper left')
 
-fig, ax, ax2 = dark_background(fig, ax, ax2)
+fig, ax = dark_background(fig, ax)
 sc.plot_save(fig, fname='h_Ra_'+regimes, fig_path=fig_path+'slides/', fig_fmt=fig_fmt, facecolor=fig.get_facecolor())
