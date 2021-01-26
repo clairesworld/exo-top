6fabfff4dd8aa1f236d9f916306ee03cd9ac2161
clairesworld <claire.guimond@gmail.com>
2021-01-26 20:11:24 +0000

    k

--- exotop/slides_plots.py
+++ exotop/slides_plots.py
@@ -31,7 +31,7 @@ elif regimes == 'chaotic':
 fig, ax = sc.plot_h_vs(Ra=Ra_ls, eta=eta_ls, t1_grid=t1_grid, end_grid=end_grid, load_grid=True, data_path=data_path,
                  fig_path=fig_path, averagescheme='timefirst', which_x='Ra_i_eff',
                  sigma=1,
-                 include_regimes=regimes, save=False, fname='h_Raieff_chaotic_timeavg', labelsize=axissize,
+                 include_regimes=include_regimes, save=False, fname='h_Raieff_chaotic_timeavg', labelsize=axissize,
                        legend=False, figsize=(16,9), showpeak=False, lw=lw, ms=25, elw=2, ecapsize=8,
                  xlabel=r'Ra$_{i,eff}$', ylabel=r'dynamic topography $\Delta h^\prime$', ylabelpad=20, xlabelpad=13,
                  title='', fiterror=False, c_fit=c_fit,
