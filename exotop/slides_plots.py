121c2e07aab4467e072fd6fa9a53908772135d52
clairesworld <claire.guimond@gmail.com>
2021-01-26 19:52:20 +0000

    k

--- exotop/slides_plots.py
+++ exotop/slides_plots.py
@@ -17,13 +17,13 @@ lw = 5
 fig, ax = sc.plot_h_vs(Ra=Ra_ls, eta=eta_ls, t1_grid=t1_grid, end_grid=end_grid, load_grid=True, data_path=data_path,
                  fig_path=fig_path, averagescheme='timefirst', p_dimensionals=None, which_x='Ra_i_eff',
                  sigma=1,
-                 include_regimes=['trans.', 'chaotic'], save=False, fname='h_Raieff_chaotic_timeavg', labelsize=axissize,
+                 include_regimes=['steady', 'trans.', 'chaotic'], save=False, fname='h_Raieff_chaotic_timeavg', labelsize=axissize,
                        legend=False, figsize=(16,9), showpeak=False, lw=lw, ms=25, elw=2, ecapsize=8,
                  xlabel=r'Ra$_{i,eff}$', ylabel=r'dynamic topography $\Delta h^\prime$', ylabelpad=20,
                  title='', fiterror=False, c_fit=c_fit,
                  c_peak='k', c_rms=c_rms,
                        fit=True, logx=True, logy=True, hscale=1,
-                 show_isoviscous=False, ylim=[6e-3, 3.5e-2], xlim=[1e6, 2e7], postprocess_kwargs=postprocess_kwargs,
+                 show_isoviscous=False, ylim=[4e-3, 3.5e-2], xlim=[1e5, 2e7], postprocess_kwargs=postprocess_kwargs,
                  regime_grid=regime_grid_td)
 
 ax.tick_params(axis='x', labelsize=ticksize, pad=15)
@@ -37,13 +37,15 @@ handles = []
 handles.append(mlines.Line2D([], [], color=c_fit, marker='*', ls='--',
                           markersize=0, lw=lw, label=r'$\Delta h = 0.094$ Ra$^{-0.151}$'))
 handles.append(mlines.Line2D([], [], color=c_rms[0], marker='o', ls='--',
-                          markersize=20, lw=0, label=r'$\Delta \eta = 10^{6}$'))
+                          markersize=20, lw=0, label=r'$\Delta \eta = 10^{5}$'))
 handles.append(mlines.Line2D([], [], color=c_rms[1], marker='o', ls='--',
-                          markersize=20, lw=0, label=r'$\Delta \eta = 10^{7}$'))
+                          markersize=20, lw=0, label=r'$\Delta \eta = 10^{6}$'))
 handles.append(mlines.Line2D([], [], color=c_rms[2], marker='o', ls='--',
+                          markersize=20, lw=0, label=r'$\Delta \eta = 10^{7}$'))
+handles.append(mlines.Line2D([], [], color=c_rms[3], marker='o', ls='--',
                           markersize=20, lw=0, label=r'$\Delta \eta = 10^{8}$'))
 
 ax.legend(handles=handles, frameon=False, fontsize=25, ncol=1, bbox_to_anchor=(1.05, 1), loc='upper left')
 
 fig, ax = dark_background(fig, ax)
-sc.plot_save(fig, fname='h_Ra_trch', fig_path=fig_path+'slides/', fig_fmt=fig_fmt, facecolor=fig.get_facecolor())
+sc.plot_save(fig, fname='h_Ra_all', fig_path=fig_path+'slides/', fig_fmt=fig_fmt, facecolor=fig.get_facecolor())
