cc8e2120a33bf7e0c07257cca63613582872f911
clairesworld <claire.guimond@gmail.com>
2021-01-27 00:06:00 +0000

    k

--- exotop/slides_plots.py
+++ exotop/slides_plots.py
@@ -52,33 +52,33 @@ for regime in regimes:
                    mlines.Line2D([], [], color=c_rms[3], marker='o', ls='--',
                                  markersize=20, lw=0, label=r'$\Delta \eta = 10^{8}$')]
 
-    # fig, ax = sc.plot_h_vs(Ra=Ra_ls, eta=eta_ls, t1_grid=t1_grid, end_grid=end_grid, load_grid=True, data_path=data_path,
-    #                  fig_path=fig_path, averagescheme='timefirst', which_x='Ra_i_eff',
-    #                  sigma=1,
-    #                  include_regimes=include_regimes, save=False,  labelsize=axissize,
-    #                        legend=False, figsize=(16,9), showpeak=False, lw=lw, ms=ms, elw=elw, ecapsize=ecapsize,
-    #                  xlabel=r'Ra$_{i,eff}$', ylabel=r'dynamic topography $\Delta h_{rms}^\prime$', ylabelpad=20, xlabelpad=13,
-    #                  title='', fiterror=False, c_fit=c_fit,
-    #                  c_peak='k', c_rms=c_rms,
-    #                        fit=True, logx=True, logy=True, hscale=1,
-    #                  show_isoviscous=False, ylim=ylim, xlim=xlim, postprocess_kwargs=postprocess_kwargs,
-    #                  regime_grid=regime_grid_td, p_dimensionals=None)
-    #
-    # ax.tick_params(axis='x', labelsize=ticksize, pad=15)
-    # ax.tick_params(axis='y', labelsize=ticksize, pad=15)
-    # if yticks is not None:
-    #     ax.set_yticks(yticks)
-    # if xticks is not None:
-    #     ax.set_xticks(xticks)
-    # ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
-    # ax.yaxis.set_minor_formatter(ticker.ScalarFormatter())
-    # # ax.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
-    # # ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.2))
-    # # ax.ticklabel_format(style='plain', axis='y', useOffset=False)
-    # ax.legend(handles=handles, frameon=False, fontsize=25, ncol=1, bbox_to_anchor=(1.01, 1), loc='upper left')
-    #
-    # fig, ax = dark_background(fig, ax)
-    # sc.plot_save(fig, fname='h_Ra_'+regime, fig_path=fig_path+'slides/', fig_fmt=fig_fmt, facecolor=fig.get_facecolor())
+    fig, ax = sc.plot_h_vs(Ra=Ra_ls, eta=eta_ls, t1_grid=t1_grid, end_grid=end_grid, load_grid=True, data_path=data_path,
+                     fig_path=fig_path, averagescheme='timefirst', which_x='Ra_i_eff',
+                     sigma=1,
+                     include_regimes=include_regimes, save=False,  labelsize=axissize,
+                           legend=False, figsize=(16,9), showpeak=False, lw=lw, ms=ms, elw=elw, ecapsize=ecapsize,
+                     xlabel=r'Ra$_{i,eff}$', ylabel=r'dynamic topography $\Delta h_{rms}^\prime$', ylabelpad=20, xlabelpad=13,
+                     title='', fiterror=False, c_fit=c_fit,
+                     c_peak='k', c_rms=c_rms,
+                           fit=True, logx=True, logy=True, hscale=1,
+                     show_isoviscous=False, ylim=ylim, xlim=xlim, postprocess_kwargs=postprocess_kwargs,
+                     regime_grid=regime_grid_td, p_dimensionals=None)
+
+    ax.tick_params(axis='x', labelsize=ticksize, pad=15)
+    ax.tick_params(axis='y', labelsize=ticksize, pad=15)
+    if yticks is not None:
+        ax.set_yticks(yticks)
+    if xticks is not None:
+        ax.set_xticks(xticks)
+    ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax.yaxis.set_minor_formatter(ticker.ScalarFormatter())
+    # ax.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
+    # ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.2))
+    # ax.ticklabel_format(style='plain', axis='y', useOffset=False)
+    ax.legend(handles=handles, frameon=False, fontsize=25, ncol=1, bbox_to_anchor=(1.01, 1), loc='upper left')
+
+    fig, ax = dark_background(fig, ax)
+    sc.plot_save(fig, fname='h_Ra_'+regime, fig_path=fig_path+'slides/', fig_fmt=fig_fmt, facecolor=fig.get_facecolor())
 
 
 
