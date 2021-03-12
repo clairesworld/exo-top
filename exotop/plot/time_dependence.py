6092eaeda4334f0cb979d4e66ebad77c96b14c11
clairesworld <claire.guimond@gmail.com>
2021-03-12 19:44:09 +0000

    k

--- exotop/plot/time_dependence.py
+++ exotop/plot/time_dependence.py
@@ -28,51 +28,68 @@ plat.subplots_hist(Ra_ls[-2:], eta_ls[-1], regime_grid=regime_grid_td[-1, -2:], 
                    cmap=cmap, nbins=10, colour_by='Ra',
                    data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
 
+plat.subplots_hist(Ra_ls[-2:], eta_ls[-2], regime_grid=regime_grid_td[-2, -2:],  t1_grid=t1_grid[-2, -2:],
+                   load_grid=True, end_grid=end_grid[-2, -2:],
+                   psuffixes=['_T', '_h'], fig_path=fig_path, save=True,
+                   fname='hist-eta1e7', fig_fmt=fig_fmt, labelsize=14,
+                   keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_rms'], title='eta 1e7', xlabelpad=8,
+                   cmap=cmap, nbins=10, colour_by='Ra',
+                   data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
+
+plat.subplots_hist(Ra_ls[-2:], eta_ls[-3], regime_grid=regime_grid_td[-3, -2:],  t1_grid=t1_grid[-3, -2:],
+                   load_grid=True, end_grid=end_grid[-3, -2:],
+                   psuffixes=['_T', '_h'], fig_path=fig_path, save=True,
+                   fname='hist-eta1e6', fig_fmt=fig_fmt, labelsize=14,
+                   keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_rms'], title='eta 1e6', xlabelpad=8,
+                   cmap=cmap, nbins=10, colour_by='Ra',
+                   data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
 
 
-# # plot evolutions for debugging T components
-
-plat.subplots_evol_at_sol(Ra_ls[-2:], eta_ls[1:], regime_grid=regime_grid_td[1:, -2:], save=True, t1_grid=t1_grid[1:, -2:],
-                          load_grid=True,
-                        psuffixes=['_T'], fig_path=fig_path,
-                        fname='evol', fig_fmt=fig_fmt, end_grid=end_grid[1:, -2:], normtime=True, labelsize=14, xlabel=r'Time',
-                        keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_components'],
-                        ylabels=['y_L', 'T_L', 'dT_rh', 'delta_rh', r'$\alpha \Delta T{_{rh} \delta_{rh}$'], title='', xlabelpad=8,
-                        ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='Ra',
-                        data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
-
-plat.subplots_evol_at_sol(Ra_ls[-2], eta_ls[1:], regime_grid=regime_grid_td[1:, -2], save=True, t1_grid=t1_grid[1:, -2], load_grid=True,
-                          psuffixes=['_T'], fig_path=fig_path,
-                          fname='evol-Ra1e8', fig_fmt=fig_fmt, end_grid=end_grid[1:,-1], normtime=True, labelsize=14, xlabel=r'Time',
-                          ylabels=None, keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
-                          ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='eta',
-                          data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
-
-
-plat.subplots_evol_at_sol(Ra_ls[-1], eta_ls[1:], regime_grid=regime_grid_td[1:, -1], save=True, t1_grid=t1_grid[1:, -1], load_grid=True,
-                          psuffixes=['_T'], fig_path=fig_path,
-                          fname='evol-Ra3e8', fig_fmt=fig_fmt, end_grid=end_grid[1:,-1], normtime=True, labelsize=14, xlabel=r'Time',
-                          ylabels=None, keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
-                          ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='eta',
-                          data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
-
-plat.subplots_evol_at_sol(Ra_ls[-2:], eta_ls[1], regime_grid=regime_grid_td[1, -2:], save=True, t1_grid=t1_grid[1,-2:],
-                        psuffixes=['_T'], fig_path=fig_path, end_grid=end_grid[1,-2:],  load_grid=True,
-                        fname='evol-eta1e6', fig_fmt=fig_fmt, normtime=True, labelsize=14, xlabel=r'Time',
-                        ylabels=None, keys=['y_L', 'T_l', 'T_i', 'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
-                        ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='Ra',
-                        data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
-
-plat.subplots_evol_at_sol(Ra_ls[-2:], eta_ls[2], regime_grid=regime_grid_td[2,-2:], save=True, t1_grid=t1_grid[2,-2:], load_grid=True,
-                        psuffixes=['_T'], fig_path=fig_path,
-                        fname='evol-eta1e7', fig_fmt=fig_fmt, end_grid=end_grid[2,-2:], normtime=True, labelsize=14, xlabel=r'Time',
-                        ylabels=None, keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
-                        ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='Ra',
-                        data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
-
-plat.subplots_evol_at_sol(Ra_ls[-2:], eta_ls[3], regime_grid=regime_grid_td[3,-2:], save=True, t1_grid=t1_grid[3,-2:], load_grid=True,
-                        psuffixes=['_T'], fig_path=fig_path,
-                        fname='evol-eta1e8', fig_fmt=fig_fmt, end_grid=end_grid[3,-2:], normtime=True, labelsize=14, xlabel=r'Time',
-                        ylabels=None, keys=['y_L', 'T_l',  'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
-                        ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='Ra',
-                        data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
+#
+#
+# # # plot evolutions for debugging T components
+#
+# plat.subplots_evol_at_sol(Ra_ls[-2:], eta_ls[1:], regime_grid=regime_grid_td[1:, -2:], save=True, t1_grid=t1_grid[1:, -2:],
+#                           load_grid=True,
+#                         psuffixes=['_T'], fig_path=fig_path,
+#                         fname='evol', fig_fmt=fig_fmt, end_grid=end_grid[1:, -2:], normtime=True, labelsize=14, xlabel=r'Time',
+#                         keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_components'],
+#                         ylabels=['y_L', 'T_L', 'dT_rh', 'delta_rh', r'$\alpha \Delta T{_{rh} \delta_{rh}$'], title='', xlabelpad=8,
+#                         ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='Ra',
+#                         data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
+#
+# plat.subplots_evol_at_sol(Ra_ls[-2], eta_ls[1:], regime_grid=regime_grid_td[1:, -2], save=True, t1_grid=t1_grid[1:, -2], load_grid=True,
+#                           psuffixes=['_T'], fig_path=fig_path,
+#                           fname='evol-Ra1e8', fig_fmt=fig_fmt, end_grid=end_grid[1:,-1], normtime=True, labelsize=14, xlabel=r'Time',
+#                           ylabels=None, keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
+#                           ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='eta',
+#                           data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
+#
+#
+# plat.subplots_evol_at_sol(Ra_ls[-1], eta_ls[1:], regime_grid=regime_grid_td[1:, -1], save=True, t1_grid=t1_grid[1:, -1], load_grid=True,
+#                           psuffixes=['_T'], fig_path=fig_path,
+#                           fname='evol-Ra3e8', fig_fmt=fig_fmt, end_grid=end_grid[1:,-1], normtime=True, labelsize=14, xlabel=r'Time',
+#                           ylabels=None, keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
+#                           ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='eta',
+#                           data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
+#
+# plat.subplots_evol_at_sol(Ra_ls[-2:], eta_ls[1], regime_grid=regime_grid_td[1, -2:], save=True, t1_grid=t1_grid[1,-2:],
+#                         psuffixes=['_T'], fig_path=fig_path, end_grid=end_grid[1,-2:],  load_grid=True,
+#                         fname='evol-eta1e6', fig_fmt=fig_fmt, normtime=True, labelsize=14, xlabel=r'Time',
+#                         ylabels=None, keys=['y_L', 'T_l', 'T_i', 'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
+#                         ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='Ra',
+#                         data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
+#
+# plat.subplots_evol_at_sol(Ra_ls[-2:], eta_ls[2], regime_grid=regime_grid_td[2,-2:], save=True, t1_grid=t1_grid[2,-2:], load_grid=True,
+#                         psuffixes=['_T'], fig_path=fig_path,
+#                         fname='evol-eta1e7', fig_fmt=fig_fmt, end_grid=end_grid[2,-2:], normtime=True, labelsize=14, xlabel=r'Time',
+#                         ylabels=None, keys=['y_L', 'T_l', 'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
+#                         ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='Ra',
+#                         data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
+#
+# plat.subplots_evol_at_sol(Ra_ls[-2:], eta_ls[3], regime_grid=regime_grid_td[3,-2:], save=True, t1_grid=t1_grid[3,-2:], load_grid=True,
+#                         psuffixes=['_T'], fig_path=fig_path,
+#                         fname='evol-eta1e8', fig_fmt=fig_fmt, end_grid=end_grid[3,-2:], normtime=True, labelsize=14, xlabel=r'Time',
+#                         ylabels=None, keys=['y_L', 'T_l',  'dT_rh', 'delta_rh', 'h_components'], title='', xlabelpad=8,
+#                         ylabelpad=8, markers=None, markersize=24, cmap=cmap, colour_by='Ra',
+#                         data_path=data_path, regime_names=regime_names_td, postprocess_kwargs=postprocess_kwargs)
