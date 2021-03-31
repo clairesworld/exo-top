a6367e81b7f65934e762393ed127687a261bbe93
clairesworld <claire.guimond@gmail.com>
2021-03-31 17:34:10 +0100

    k

--- exotop/plot/psd.py
+++ exotop/plot/psd.py
@@ -23,20 +23,19 @@ fig, ax = plat.plot_norm_spectra(Ra_ls, eta_ls, cmap='rainbow', end_grid=end_gri
 
 """ get all time-averaged spectra and store """
 
-
-for ii, eta in enumerate(eta_ls):  # across eta_ls
-    cases_ii = ['Ra' + Ra + '-eta' + eta + e for Ra, e in zip(Ra_ls, end_grid[ii])]
-    labels_ii = ['Ra=' + Ra for Ra in Ra_ls]
-    for jj, Ra in enumerate(Ra_ls):
-        if regime_grid_td[ii, jj] in regimes_use:
-            case = cases_ii[jj]
-            t1 = t1_grid[ii, jj]
-            print('Calculating spectrum for', case)
-            fig, ax = sh.dct_spectrum_avg(case, L_x=8,
-                                          dim=False, R_p=d, d=d, dT=dT, alpha=alpha,
-                                          t0=t1, x_res=1, t_res=1,
-                                          test=False, data_path=data_path, fig_path=fig_path,
-                                          check_norm=False,
-                                          plot=True, load=False, dump=True, save=True, y0_guide=1e0,
-                                          )
-            print('    ...finished!')
+# for ii, eta in enumerate(eta_ls):  # across eta_ls
+#     cases_ii = ['Ra' + Ra + '-eta' + eta + e for Ra, e in zip(Ra_ls, end_grid[ii])]
+#     labels_ii = ['Ra=' + Ra for Ra in Ra_ls]
+#     for jj, Ra in enumerate(Ra_ls):
+#         if regime_grid_td[ii, jj] in regimes_use:
+#             case = cases_ii[jj]
+#             t1 = t1_grid[ii, jj]
+#             print('Calculating spectrum for', case)
+#             fig, ax = sh.dct_spectrum_avg(case, L_x=8,
+#                                           dim=False, R_p=d, d=d, dT=dT, alpha=alpha,
+#                                           t0=t1, x_res=1, t_res=1,
+#                                           test=False, data_path=data_path, fig_path=fig_path,
+#                                           check_norm=False,
+#                                           plot=True, load=False, dump=True, save=True, y0_guide=1e0,
+#                                           )
+#             print('    ...finished!')
