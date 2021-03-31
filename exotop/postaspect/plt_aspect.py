a78799dc7823ef8e7cee79fe7dc514b5d754a5a5
clairesworld <claire.guimond@gmail.com>
2021-03-31 18:55:04 +0100

    k

--- exotop/postaspect/plt_aspect.py
+++ exotop/postaspect/plt_aspect.py
@@ -2028,72 +2028,72 @@ def plot_norm_spectra(Ra_ls, eta_ls, cmap='rainbow', end_grid=None, regime_grid=
 
     if fig is None and ax is None:
         fig, ax = plt.subplots(1, 1, figsize=figsize)
-    if z_name == 'Ra':
-        z_vec = [np.log10(float(z)) for z in Ra_ls]
-    elif z_name == 'eta':
-        z_vec = [np.log10(float(z)) for z in eta_ls]
-    elif z_name == 'Ra_i_eff':
-        z_grid = np.zeros((len(eta_ls), len(Ra_ls)), dtype=np.float64)
-        for jj, eta_str in enumerate(eta_ls):
-            cases, Ra_var = pro.get_cases_list(Ra_ls, eta_str, end_grid[jj])
-            for ii, case in enumerate(cases):
-                if regime_grid[jj][ii] in include_regimes:
-                    fname = data_path + 'output-' + case + '/pickle/' + case + pend + fend
-                    if os.path.isfile(fname):
-                        zn = plot_getx(Ra_ls[ii], eta_str, case=case, which_x='Ra_i_eff', averagescheme='timefirst',
-                                       data_path=data_path, load=True, postprocess_kwargs=None, return_all=False, t1=0,
-                                       alpha_m=alpha_m, **kwargs)
-                        print('zn', zn)
-                        z_grid[jj, ii] = zn
-        z_vec = z_grid.flatten()
 
-    clist = colorize(z_vec, cmap=cmap, vmin=vmin, vmax=vmax)[0]
+    # get colouring
 
-    # load spectra
+    ji_use = []
+    z_vec = []
     for jj, eta_str in enumerate(eta_ls):
         cases, Ra_var = pro.get_cases_list(Ra_ls, eta_str, end_grid[jj])
         for ii, case in enumerate(cases):
             if regime_grid[jj][ii] in include_regimes:
                 fname = data_path + 'output-' + case + '/pickle/' + case + pend + fend
                 if os.path.isfile(fname):
+                    ji_use.append((jj, ii))
                     if z_name == 'Ra':
-                        zz = ii
+                        z_vec.append(np.log10(float(Ra_ls[ii])))
                     elif z_name == 'eta':
-                        zz = jj
+                        z_vec.append(np.log10(float(eta_str)))
                     elif z_name == 'Ra_i_eff':
-                        zz = np.ravel_multi_index((jj, ii), np.shape(z_grid))
-                        print('zz', zz)
-
-                    S, k = pkl.load(open(fname, "rb"))
-                    if k[0] == 0:  # only wavenumbers greater than 0
-                        k = k[1:]
-                        S = S[1:]
-                    l = sh.k_to_l(k, R=d)  # should be l=1.9674 at the top
-
-                    # wavenumber range where spectrum makes sense
-                    wl_min, wl_max = sh.nat_scales(case, ax=None, alpha=alpha, d=d, dim=dim, data_path=data_path, **kwargs)
-                    k_min, k_max = 1 / wl_max, 1 / wl_min
-                    if k_min is not None and (k_min > np.min(k)):
-                        i_min = np.argmax(k >= k_min)
-                    if k_max is not None and (k_max < np.max(k)):
-                        i_max = np.argmax(k >= k_max)
-                    kv = k[i_min:i_max + 1]
-                    Sv = S[i_min:i_max + 1]
-
-                    if norm == 'min_l':
-                        Sv_norm = Sv / Sv[0]  # actually normalise to first point inside k range
-                        S_norm = Sv / Sv[0]
-                    elif norm == 'k2':
-                        Sv_norm = Sv / Sv[0] * kv**2  # trying to emphasise k**-2 slope but doesn't rlly work
-                    elif norm == 'intercept':
-                        beta, intercept = sh.fit_slope(Sv, kv, k_min=None, k_max=None, plot=False)
-                        Sv_norm = Sv / intercept
-
-                    ax.plot(kv, Sv_norm, c=clist[zz], alpha=1, lw=lw, marker=marker)
-                    # ax.plot(k, S_norm, c=clist[zz], alpha=0.1, lw=lw, marker=marker)  # not in range
-
+                        z_vec.append(plot_getx(Ra_ls[ii], eta_str, case=case, which_x='Ra_i_eff', averagescheme='timefirst',
+                                       data_path=data_path, load=True, postprocess_kwargs=None, return_all=False, t1=0,
+                                       alpha_m=alpha_m, **kwargs))
                 else:
                     print(fname, 'not found')
+    clist = colorize(z_vec, cmap=cmap, vmin=vmin, vmax=vmax)[0]
+
+    # load spectra
+    zz = 0
+    for (jj, ii) in ji_use:
+        case = 'Ra' + Ra_ls[ii] + '-eta' + eta_ls[jj] + end_grid[jj, ii]
+        print('case', case)
+        fname = data_path + 'output-' + case + '/pickle/' + case + pend + fend
+
+        # if z_name == 'Ra':
+        #     zz = ix
+        # elif z_name == 'eta':
+        #     zz = jx
+        # elif z_name == 'Ra_i_eff':
+        #     zz = np.ravel_multi_index((jj, ii), np.shape(z_grid))
+        #     print('zz', zz)
+
+        S, k = pkl.load(open(fname, "rb"))
+        if k[0] == 0:  # only wavenumbers greater than 0
+            k = k[1:]
+            S = S[1:]
+        l = sh.k_to_l(k, R=d)  # should be l=1.9674 at the top
+
+        # wavenumber range where spectrum makes sense
+        wl_min, wl_max = sh.nat_scales(case, ax=None, alpha=alpha, d=d, dim=dim, data_path=data_path, **kwargs)
+        k_min, k_max = 1 / wl_max, 1 / wl_min
+        if k_min is not None and (k_min > np.min(k)):
+            i_min = np.argmax(k >= k_min)
+        if k_max is not None and (k_max < np.max(k)):
+            i_max = np.argmax(k >= k_max)
+        kv = k[i_min:i_max + 1]
+        Sv = S[i_min:i_max + 1]
+
+        if norm == 'min_l':
+            Sv_norm = Sv / Sv[0]  # actually normalise to first point inside k range
+            S_norm = Sv / Sv[0]
+        elif norm == 'k2':
+            Sv_norm = Sv / Sv[0] * kv**2  # trying to emphasise k**-2 slope but doesn't rlly work
+        elif norm == 'intercept':
+            beta, intercept = sh.fit_slope(Sv, kv, k_min=None, k_max=None, plot=False)
+            Sv_norm = Sv / intercept
+
+        ax.plot(kv, Sv_norm, c=clist[zz], alpha=1, lw=lw, marker=marker)
+        # ax.plot(k, S_norm, c=clist[zz], alpha=0.1, lw=lw, marker=marker)  # not in range
 
     ax.loglog()
     ax.set_xlabel(xlabel, fontsize=labelsize)
