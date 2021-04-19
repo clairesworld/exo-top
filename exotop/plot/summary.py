9f7dd23e277d176039c4fa7777e67799f4829289
clairesworld <claire.guimond@gmail.com>
2021-04-19 14:58:23 +0100

    k

--- exotop/plot/summary.py
+++ exotop/plot/summary.py
@@ -50,7 +50,7 @@ from postaspect import aspect_post as ap
 plat.subplots_cases(
     ['Lees-Ra1e6-2D'], labels=['2D isoviscous benchmark'], t1=[0.5], save=True, load=False,
     fname='Lees-benchmark', suptitle='Ra = 1e6', c_rms=c_rms, c_peak=c_peak, dt_xlim=(0.0, 1.0),
-    includepdf=True, includeTz=True, show_sols=True,  # set False for faster summary with stats only
+    includepdf=True, includeTz=False, show_sols=True,  # set False for faster summary with stats only
     includegraphic=True, data_path=data_path, fig_path=fig_path, fig_fmt=fig_fmt,
     postprocess_kwargs=postprocess_kwargs,
 )
