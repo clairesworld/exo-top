83fea58ef87ff547f257fc96b0ead65d04cbdf0e
clairesworld <claire.guimond@gmail.com>
2021-04-28 20:12:51 +0100

    k

--- exotop/plot/regimes.py
+++ exotop/plot/regimes.py
@@ -1,6 +1,7 @@
 from postaspect.setup_postprocessing import Ra_ls, eta_ls, t1_grid, end_grid, data_path, fig_path, c_rms, c_peak, c_regimes_td, \
     fig_fmt, regime_grid_td, regime_names_td, load_grid, alpha_m
 from postaspect import plt_aspect as plat
+from postaspect import aspect_post as ap
 
 
 load = True  # load_grid
@@ -18,7 +19,7 @@ load = True  # load_grid
 #                          )
 
 # lid mobility
-plat.plot_parameter_grid(Ra_ls, eta_ls, function=plat.surf_mobility_at_sol, regime_grid=regime_grid_td,
+plat.plot_parameter_grid(Ra_ls, eta_ls, function=ap.surf_mobility_at_sol, regime_grid=regime_grid_td,
                        data_path=data_path, fig_path=fig_path, load=True, vmax=1, log=True, clabel='log surface mobility',
                        title='Stagnant lid regime',
                        save=True, fname='surface-mobility', labelsize=16, fig_fmt=fig_fmt, t1=t1_grid, end=end_grid,
